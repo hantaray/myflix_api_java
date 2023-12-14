06c15cf38d7c39cf4faa1cc85d15e2ef1ef7a1a0
hansashi <ahantke@mail.de>
2023-12-14 19:20:20 +0100

    Initial commit

---
+++ src/main/java/com/hantaray/myflixAPI/MyflixApiApplication.java
@@ -0,0 +1,13 @@
+package com.hantaray.myflixAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyflixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyflixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hantaray/myflixAPI/MyflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hantaray.myflixAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyflixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
