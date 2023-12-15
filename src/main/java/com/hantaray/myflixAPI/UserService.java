738ce03f6a0e446bd7e201031be850da73b90b01
hansashi <ahantke@mail.de>
2023-12-15 01:08:14 +0100

    created UserController

---
+++ src/main/java/com/hantaray/myflixAPI/UserService.java
@@ -0,0 +1,15 @@
+package com.hantaray.myflixAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> allUser(){
+        return userRepository.findAll();
+    }
+}
